7bafd28d34f2a4452b129c58baa8c5aede9f46a9
donjd <99505859+donjd@users.noreply.github.com>
2024-11-20 07:22:19 -0700

    Start writing javascript for game

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("test");
+let tries = 0;
+let userInput = prompt("Choose rock, paper, or scissors").toLowerCase;
+let computerAnswer;
+
+function generateRockPaperScissors() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber == 1) {
+    computerAnswer = "rock";
+  } else if (randomNumber == 2) {
+    computerAnswer = "paper";
+  } else {
+    computerAnswer = "scissors";
+  }
+  return computerAnswer;
+}
+
+generateRockPaperScissors();
+
+if (userInput == computerAnswer) {
+  tries += 1;
+}
+console.log(tries);
