07b3ae7b95ac771988d7fa9e1b8766d6a577fc21
donjd <99505859+donjd@users.noreply.github.com>
2024-11-23 20:48:25 -0700

    Finished game!

--- main.js
+++ main.js
@@ -1,55 +1,107 @@
-let computerAnswer = "";
-let userInput = "";
-let userScore = 0;
+let humanChoice = "";
+let computerChoice = "";
+
+let humanScore = 0;
 let computerScore = 0;
 
-function getUserInput() {
-  userInput = prompt("Choose rock, paper, or scissors");
+function getHumanChoice() {
+  humanChoice = prompt("Choose rock, paper, or scissors");
 }
 
-function getComputerAnswer() {
+function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber == 1) {
-    computerAnswer = "rock";
+    computerChoice = "rock";
   } else if (randomNumber == 2) {
-    computerAnswer = "paper";
+    computerChoice = "paper";
   } else {
-    computerAnswer = "scissors";
+    computerChoice = "scissors";
   }
-  return computerAnswer;
+  return computerChoice;
 }
 
-function getWinner() {
-  let roundResult = `You: ${userInput}, Computer: ${computerAnswer}`;
-  let totalScore = `You: ${userScore}, Computer: ${computerScore}`;
-
-  if (userInput == "rock" && computerAnswer == "paper") {
-    computerScore++;
-    alert(`you lost. Round Result: ${roundResult}, Total Score: ${totalScore}`);
-  } else if (userInput == "rock" && computerAnswer == "scissors") {
-    userScore++;
-    alert(`you won! Round Result: ${roundResult}, Total Score: ${totalScore}`);
-  } else if (userInput == "paper" && computerAnswer == "rock") {
-    userScore++;
-    alert(`you won! Round Result: ${roundResult}, Total Score: ${totalScore}`);
-  } else if (userInput == "paper" && computerAnswer == "scissors") {
-    computerScore++;
-    alert(`you lost. Round Result: ${roundResult}, Total Score: ${totalScore}`);
-  } else if (userInput == "scissors" && computerAnswer == "rock") {
-    computerScore++;
-    alert(`you lost. Round Result: ${roundResult}, Total Score: ${totalScore}`);
-  } else if (userInput == "scissors" && computerAnswer == "paper") {
-    userScore++;
-    alert(`you won! Round Result: ${roundResult}, Total Score: ${totalScore}`);
-  } else if (userInput == computerAnswer) {
-    alert(
-      `It's a tie, go again! Round Result: ${roundResult}, Total Score: ${totalScore}`
-    );
+function playRound() {
+  switch (humanChoice) {
+    case "rock":
+      switch (computerChoice) {
+        case humanChoice:
+          alert(
+            `It's a tie! You chose: ${humanChoice}, Computer chose: ${computerChoice}. Your Score: ${humanScore}, Computer Score: ${computerScore}`
+          );
+          break;
+        case "paper":
+          computerScore++;
+          alert(
+            `You lost! You chose: ${humanChoice}, Computer chose: ${computerChoice}. Your Score: ${humanScore}, Computer Score: ${computerScore}`
+          );
+          break;
+        case "scissors":
+          humanScore++;
+          alert(
+            `You won! You chose: ${humanChoice}, Computer chose: ${computerChoice}. Your Score: ${humanScore}, Computer Score: ${computerScore}`
+          );
+          break;
+      }
+      break;
+    case "paper":
+      switch (computerChoice) {
+        case humanChoice:
+          alert(
+            `It's a tie! You chose: ${humanChoice}, Computer chose: ${computerChoice}. Your Score: ${humanScore}, Computer Score: ${computerScore}`
+          );
+          break;
+        case "rock":
+          humanScore++;
+          alert(
+            `You won! You chose: ${humanChoice}, Computer chose: ${computerChoice}. Your Score: ${humanScore}, Computer Score: ${computerScore}`
+          );
+          break;
+        case "scissors":
+          computerScore++;
+          alert(
+            `You lost! You chose: ${humanChoice}, Computer chose: ${computerChoice}. Your Score: ${humanScore}, Computer Score: ${computerScore}`
+          );
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerChoice) {
+        case humanChoice:
+          alert(
+            `It's a tie! You chose: ${humanChoice}, Computer chose: ${computerChoice}. Your Score: ${humanScore}, Computer Score: ${computerScore}`
+          );
+          break;
+        case "rock":
+          computerScore++;
+          alert(
+            `You lost! You chose: ${humanChoice}, Computer chose: ${computerChoice}. Your Score: ${humanScore}, Computer Score: ${computerScore}`
+          );
+          break;
+        case "paper":
+          humanScore++;
+          alert(
+            `You won! You chose: ${humanChoice}, Computer chose: ${computerChoice}. Your Score: ${humanScore}, Computer Score: ${computerScore}`
+          );
+          break;
+      }
+      break;
   }
 }
 
-for (i = 0; i < 10; i++) {
-  getUserInput();
-  getComputerAnswer();
-  getWinner();
+function playGame() {
+  for (i = 0; i < 5; i++) {
+    getHumanChoice();
+    getComputerChoice();
+    playRound();
+  }
+
+  if (humanScore > computerScore) {
+    alert(`You won! The final score was ${humanScore}:${computerScore}`);
+  } else if (computerScore > humanScore) {
+    alert(`You lost! The final score was ${humanScore}:${computerScore}`);
+  } else {
+    alert(`You tied! The final score was ${humanScore}:${computerScore}`);
+  }
 }
+
+playGame();
