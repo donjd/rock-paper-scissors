54c32e492580889fbe4d3885d39d10d92124801d
donjd <99505859+donjd@users.noreply.github.com>
2024-12-11 20:41:26 -0700

    Simplify code to test listen to enter key

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
-const commandLine = document.querySelector(".command-line");
+const initialInput = document.querySelector(".initialInput");
 
-function runUserCommand() {
-  const createNewLine = function (e) {
-    if (e.key == "Enter") {
-      const inputSection = document.createElement("div");
-      const prompt = document.createElement("p");
-      const userInput = document.createElement("input");
-
-      userInput.setAttribute("type", "text");
-
-      inputSection.setAttribute(
-        "style",
-        "display: flex; gap: 10px; align-items: center;"
-      );
-      prompt.textContent = "user:~/rock-paper-scissors$";
-      prompt.setAttribute("style", "flex: 0 0 auto;");
-
-      userInput.setAttribute("style", "flex: 1 1 auto;");
-
-      inputSection.appendChild(prompt);
-      inputSection.appendChild(userInput);
-      commandLine.appendChild(inputSection);
-
-      userInput.focus();
-
-      userInput.addEventListener("keydown", createNewLine);
-    }
-  };
-
-  const initialInput = document.querySelector(".initialInput");
-
-  initialInput.addEventListener("keydown", createNewLine);
-}
-
-runUserCommand();
+initialInput.addEventListener("keyup", (e) => {
+  if (e.key == "enter") {
+    console.log("it worked");
+  }
+});
