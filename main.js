362482f6ab47e7a0cb426d8503bb2ce16a0d1f9d
donjd <99505859+donjd@users.noreply.github.com>
2024-12-11 21:45:41 -0700

    Typing commands in input logs to console

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
-const initialInput = document.querySelector(".initialInput");
+const commandLine = document.querySelector(".command-line");
+const initialInput = document.querySelector("#initialInput");
+let userInput = initialInput;
 
 initialInput.addEventListener("keyup", (e) => {
-  if (e.key == "enter") {
-    console.log("it worked");
+  if (e.key == "Enter") {
+    //output parent
+    const inputSection = document.createElement("div");
+    inputSection.classList.add("inputSection");
+
+    //output children
+    const output = document.createElement("p");
+    output.classList.add("output");
+
+    //prompt section parent
+    const promptSection = document.createElement("div");
+    promptSection.classList.add("promptSection");
+
+    //prompt section children
+    const prompt = document.createElement("p");
+    prompt.classList.add("prompt");
+    const input = document.createElement("input");
+    input.classList.add("input");
+
+    //styling
+    inputSection.setAttribute("style", "display");
+
+    //adding content
+    output.textContent = runCommands(userInput.value);
+    prompt.textContent = "user:~/rock-paper-scissors$";
+
+    inputSection.appendChild(output);
+    inputSection.appendChild(promptSection);
+    promptSection.appendChild(prompt);
+    promptSection.appendChild(input);
+    commandLine.appendChild(inputSection);
   }
 });
+
+function runCommands(cmd) {
+  switch (cmd) {
+    case "rps --help":
+      console.log("this worke yo");
+      break;
+    case "rps rock":
+      console.log("rocks");
+      break;
+  }
+}
